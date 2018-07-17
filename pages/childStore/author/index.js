9d70918d102cdd8ffe093b66dd5529d3962d4519
Elderly <897676943@qq.com>
2018-07-17 22:22:59 +0800

    童言无忌

---
+++ pages/childStore/author/index.js
@@ -0,0 +1,102 @@
+// pages/childStore/author/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    List:[
+      {
+        id:1,
+        type:'绘本',
+        name:'宝宝第一次说话12312312312312312',
+        author:'宝妈31231231231231231232312312312312312',
+        label:['可爱','童话123123123']
+      },
+      {
+        id: 2,
+        type: '原创',
+        name: '宝宝第一次说话',
+        author: '宝妈',
+        label: ['可爱12312312', '童话', '可爱', '童话']
+      },
+      {
+        id: 3,
+        type: '绘本',
+        name: '宝宝第一次说话',
+        author: '宝妈',
+        label: ['可爱', '童话']
+      },
+      {
+        id: 4,
+        type: '绘本',
+        name: '宝宝第一次说话',
+        author: '宝妈',
+        label: ['可爱', '童话']
+      },
+      {
+        id: 1,
+        type: '绘本',
+        name: '宝宝第一次说话',
+        author: '宝妈',
+        label: ['可爱', '童话']
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
--- pages/childStore/index/index.js
+++ pages/childStore/index/index.js
@@ -62,5 +62,11 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+
+  GoAuthor:function(){
+    wx.navigateTo({
+      url: '../author/index'
+    })
   }
 })
